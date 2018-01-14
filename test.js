24b3119d7a5a8f4a10079efdaeb8ff08c08bbe9e
Lufty Wiranda <lufty.wiranda@gmail.com>
2018-01-15 05:48:53 +0700

    refactor: experiment

--- test.js
+++ test.js
@@ -16,22 +16,24 @@ const setupRepo = async cwd => {
 
 describe('in temp dir', () => {
   it('deletes folder', async () => {
-    expect.assertions(1);
+    expect.assertions(2);
     const tmpPath = f.copy('fixtures');
 
     await setupRepo(tmpPath);
     await forceDel('nested', { cwd: tmpPath });
 
+    expect(pathExistsSync(tmpPath)).toBe(true);
     expect(pathExistsSync(join(tmpPath, 'nested'))).toBe(false);
   });
 
   it('deletes files from git repo', async () => {
-    expect.assertions(3);
+    expect.assertions(4);
     const tmpPath = f.copy('fixtures');
 
     await setupRepo(tmpPath);
     await forceDel('**/*.txt', { cwd: tmpPath });
 
+    expect(pathExistsSync(tmpPath)).toBe(true);
     expect(pathExistsSync(join(tmpPath, 'bar.txt'))).toBe(false);
     expect(pathExistsSync(join(tmpPath, 'file.txt'))).toBe(false);
     expect(pathExistsSync(join(tmpPath, 'nested', 'nested-file.txt'))).toBe(
@@ -40,11 +42,12 @@ describe('in temp dir', () => {
   });
 
   it('deletes files from general file-system', async () => {
-    expect.assertions(3);
+    expect.assertions(4);
     const tmpPath = f.copy('fixtures');
 
     await forceDel('**/*.txt', { cwd: tmpPath });
 
+    expect(pathExistsSync(tmpPath)).toBe(true);
     expect(pathExistsSync(join(tmpPath, 'bar.txt'))).toBe(false);
     expect(pathExistsSync(join(tmpPath, 'file.txt'))).toBe(false);
     expect(pathExistsSync(join(tmpPath, 'nested', 'nested-file.txt'))).toBe(
@@ -62,24 +65,26 @@ describe('in `process.cwd()`', () => {
   });
 
   it('deletes folder', async () => {
-    expect.assertions(1);
+    expect.assertions(2);
     const tmpPath = f.copy('fixtures');
     process.chdir(tmpPath);
 
     await setupRepo(tmpPath);
     await forceDel('nested');
 
+    expect(pathExistsSync(tmpPath)).toBe(true);
     expect(pathExistsSync(join(tmpPath, 'nested'))).toBe(false);
   });
 
   it('deletes files from git repo', async () => {
-    expect.assertions(3);
+    expect.assertions(4);
     const tmpPath = f.copy('fixtures');
     process.chdir(tmpPath);
 
     await setupRepo(tmpPath);
     await forceDel('**/*.txt');
 
+    expect(pathExistsSync(tmpPath)).toBe(true);
     expect(pathExistsSync(join(tmpPath, 'bar.txt'))).toBe(false);
     expect(pathExistsSync(join(tmpPath, 'file.txt'))).toBe(false);
     expect(pathExistsSync(join(tmpPath, 'nested', 'nested-file.txt'))).toBe(
@@ -88,12 +93,13 @@ describe('in `process.cwd()`', () => {
   });
 
   it('deletes files from general file-system', async () => {
-    expect.assertions(3);
+    expect.assertions(4);
     const tmpPath = f.copy('fixtures');
     process.chdir(tmpPath);
 
     await forceDel('**/*.txt');
 
+    expect(pathExistsSync(tmpPath)).toBe(true);
     expect(pathExistsSync(join(tmpPath, 'bar.txt'))).toBe(false);
     expect(pathExistsSync(join(tmpPath, 'file.txt'))).toBe(false);
     expect(pathExistsSync(join(tmpPath, 'nested', 'nested-file.txt'))).toBe(
