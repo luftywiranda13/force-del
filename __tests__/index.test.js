f47c989f61aa0a2ca51e8fbf053c38d7e5209dbb
Lufty Wiranda <lufty.wiranda@gmail.com>
2018-01-19 04:17:17 +0700

    test: no need to manipulate `process.cwd()` anymore

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const { join } = require('path');
 const { pathExists } = require('fs-extra');
 const execa = require('execa');
 const fixtures = require('fixturez');
@@ -11,82 +12,67 @@ const forceDel = require('../');
 const f = fixtures(__dirname);
 
 describe('git repo', () => {
-  const realCWD = process.cwd();
   let tmpPath;
 
   beforeEach(async () => {
     tmpPath = f.copy('fixtures');
-    sgf.cwd = tmpPath;
-
-    process.chdir(tmpPath);
-    await execa('git', ['init']);
-  });
-
-  afterEach(() => {
-    sgf.cwd = realCWD;
-    process.chdir(realCWD);
+    await execa('git', ['init'], { cwd: tmpPath });
   });
 
   it('deletes new directory', async () => {
     expect.assertions(2);
 
-    await execa('git', ['add', 'foo']);
-    await forceDel('foo');
+    await execa('git', ['add', 'foo'], { cwd: tmpPath });
+    await forceDel('foo', { cwd: tmpPath });
 
-    // No files are tracked.
-    // So, Staging area should not list anything
+    sgf.cwd = tmpPath;
     await expect(pify(sgf)()).resolves.toEqual([]);
-    await expect(pathExists('foo')).resolves.toBe(false);
+    await expect(pathExists(join(tmpPath, 'foo'))).resolves.toBe(false);
   });
 
   it('deletes new files', async () => {
     expect.assertions(1);
 
-    await execa('git', ['add', '--all']);
-    await forceDel('**/*');
+    await execa('git', ['add', '--all'], { cwd: tmpPath });
+    await forceDel('**/*', { cwd: tmpPath });
 
+    sgf.cwd = tmpPath;
     await expect(pify(sgf)()).resolves.toEqual([]);
   });
 
   it('deletes committed files', async () => {
     expect.assertions(1);
 
-    await execa('git', ['add', '--all']);
-    await execa('git', ['commit', '-m', 'initial commit']);
-    await forceDel('**/*');
+    await execa('git', ['add', '--all'], { cwd: tmpPath });
+    await execa('git', ['commit', '-m', 'initial commit'], { cwd: tmpPath });
+    await forceDel('**/*', { cwd: tmpPath });
 
     // Staging area should list files marked as `deleted`
+    sgf.cwd = tmpPath;
     await expect(pify(sgf)()).resolves.not.toEqual([]);
   });
 });
 
 describe('general file-system', () => {
-  const realCWD = process.cwd();
   let tmpPath;
 
   beforeEach(async () => {
     tmpPath = f.copy('fixtures');
-
-    process.chdir(tmpPath);
-  });
-
-  afterEach(() => {
-    process.chdir(realCWD);
   });
 
   it('deletes directory', async () => {
     expect.assertions(1);
 
-    await forceDel('foo');
+    await forceDel('foo', { cwd: tmpPath });
 
-    await expect(pathExists('foo')).resolves.toBe(false);
+    await expect(pathExists(join(tmpPath, 'foo'))).resolves.toBe(false);
   });
 
   it('deletes files', async () => {
     expect.assertions(1);
 
-    await forceDel('**/*');
+    await forceDel('**/*', { cwd: tmpPath });
 
-    await expect(pathExists('foo')).resolves.toBe(false);
+    await expect(pathExists(join(tmpPath, 'foo'))).resolves.toBe(false);
   });
 });
