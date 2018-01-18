99054826b49d82b391b09faacb8353ec4fe4150d
Lufty Wiranda <lufty.wiranda@gmail.com>
2018-01-18 22:39:51 +0700

    test: write rejection test

---
+++ error.test.js
@@ -0,0 +1,26 @@
+'use strict';
+
+jest.mock('execa');
+
+const execa = require('execa');
+const fixtures = require('fixturez');
+
+const forceDel = require('./');
+
+const f = fixtures(__dirname);
+
+describe('errors', () => {
+  afterEach(() => {
+    execa.mockClear();
+  });
+
+  it("doesn't handle non-git related errors", async () => {
+    expect.assertions(1);
+
+    // eslint-disable-next-line prefer-promise-reject-errors
+    execa.mockImplementation(() => Promise.reject({ stderr: 'Oops' }));
+    const tmpPath = f.copy('fixtures');
+
+    await expect(forceDel('foo', { cwd: tmpPath })).rejects.toThrow();
+  });
+});
