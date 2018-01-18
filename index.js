93a70554d9dc6475842eaadf6ae7647cac6e6cb3
Lufty Wiranda <lufty.wiranda@gmail.com>
2018-01-18 20:43:18 +0700

    refactor: reorganize defaults (#13)
    * refactor(options): move defaults to be inside the main function scope
    
    * refactor: remove leftover del's `force` option

--- index.js
+++ index.js
@@ -9,14 +9,13 @@ const rimraf = require('rimraf');
 
 const rimrafP = pify(rimraf);
 
-const DEFAULTS = { nodir: false, force: true };
-
 const gitForceRemove = (file, options) =>
   execa('git', ['rm', '-rf', file], options)
     // TODO: handle specific `git` errors
     .catch(() => rimrafP(file, { glob: false }));
 
 const forceDel = (patterns, options) => {
+  const DEFAULTS = { nodir: false };
   const opts = Object.assign({}, DEFAULTS, options);
 
   const mapper = file => {
