814815a3160f63d8bf2c2e8faa11bc4b420be92b
Lufty Wiranda <lufty.wiranda@gmail.com>
2018-01-20 05:40:11 +0700

    fix: show more user-friendly error when process limit exceeded (#16)
    `spawn git EAGAIN` error message

--- __tests__/error.test.js
+++ __tests__/error.test.js
@@ -14,6 +14,18 @@ describe('error', () => {
     execa.mockClear();
   });
 
+  it('throws if process limit exceeded', async () => {
+    expect.assertions(1);
+
+    execa.mockImplementation(() => {
+      return Promise.reject(new Error('spawn git EAGAIN'));
+    });
+
+    const tmpPath = f.copy('fixtures');
+
+    await expect(forceDel('foo', { cwd: tmpPath })).rejects.toThrow();
+  });
+
   it("doesn't handle non-git related error", async () => {
     expect.assertions(1);
 
