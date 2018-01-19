de6f221cbab5d8946bb64ba730c26022ceca2f68
Lufty Wiranda <lufty.wiranda@gmail.com>
2018-01-20 05:19:25 +0700

    fix: show more user-friendly error when process limit exceeded

--- index.js
+++ index.js
@@ -1,40 +1,28 @@
 'use strict';
 
 const { resolve } = require('path');
-const execa = require('execa');
+
 const globby = require('globby');
-const pify = require('pify');
 const pMap = require('p-map');
-const rimraf = require('rimraf');
 
-const rimrafP = pify(rimraf);
+const deleteFile = require('./lib/delete-file');
 
-const gitForceRemove = (file, options) =>
-  execa('git', ['rm', '-rf', file], options).catch(err => {
-    const stderr = err.stderr;
-
-    if (
-      // TODO: handle error.message = "spawn git EAGAIN"
-      stderr.startsWith('fatal: Not a git repository') ||
-      stderr.startsWith('fatal: pathspec')
-    ) {
-      return Promise.resolve(rimrafP(file, { glob: false }));
-    }
-
-    return Promise.reject(stderr);
-  });
-
-const forceDel = (patterns, options) => {
-  const DEFAULTS = { nodir: false, cwd: process.cwd() };
-  const opts = Object.assign({}, DEFAULTS, options);
-
-  const mapper = file => {
-    const resolvedFile = resolve(opts.cwd, file);
-
-    return gitForceRemove(resolvedFile, opts).then(() => resolvedFile);
+const optionsManager = options => {
+  const DEFAULTS = {
+    nodir: false,
+    cwd: process.cwd(),
   };
 
-  return globby(patterns, opts).then(files => pMap(files, mapper, opts));
+  return Object.assign({}, DEFAULTS, options);
+};
+
+const forceDel = (patterns, options) => {
+  const opts = optionsManager(options);
+  const mapper = file => deleteFile(file, opts);
+
+  return globby(patterns, opts)
+    .then(files => files.map(x => resolve(opts.cwd, x)))
+    .then(files => pMap(files, mapper, opts));
 };
 
 module.exports = forceDel;
